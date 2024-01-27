b02d5279c6b4a17516c6a556ad7ae27770b6922c
Andy Cormack <incurse@gmail.com>
2024-01-27 02:10:32 +0000

    feat: basic border radius demo

--- src/routes/tier-1/border-radius-previewer/+page.svelte
+++ src/routes/tier-1/border-radius-previewer/+page.svelte
@@ -1,4 +1,26 @@
 <script lang="ts">
+  let amount = $state(20)
+  let unit = $state('px')
+  let borderRadius = $derived(`${amount}${unit}`)
 </script>
 
 <div class="text-xl">Border-radius Previewer</div>
+
+<div class="card bg-emerald-500 m-8" style:border-radius={borderRadius}>
+  <div class="card-body">
+    <label class="form-control">
+      <div class="label">
+        <span class="label-text">Border Radius</span>
+      </div>
+
+      <div>
+        <input class="input" type="number" bind:value={amount} />
+
+        <select class="select" bind:value={unit}>
+          <option value="px">px</option>
+          <option value="%">%</option>
+        </select>
+      </div>
+    </label>
+  </div>
+</div>
