69f7c32e63083b0b60f7b509e2f5cf473ba4787d
Andy Cormack <incurse@gmail.com>
2024-01-27 01:28:39 +0000

    feat: better header nav to group tiers

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,22 @@
   import '../app.css'
 </script>
 
-<header>
-  <a href="/" class="btn btn-ghost btn-sm">Home</a>
+<header class="navbar bg-base-100">
+  <div class="navbar-start">
+    <div class="dropdown">
+      <div tabindex="0" role="button" class="btn btn-ghost">Tier 1</div>
 
-  <a href="/tier-1/bin2dec" class="btn btn-ghost btn-sm">Bin2Dec</a>
+      <ul
+        class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow shadow-black/50 bg-base-100 rounded-box w-52"
+      >
+        <li><a href="/tier-1/bin2dec">Bin2Dec</a></li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="navbar-center">
+    <a href="/" class="btn btn-ghost text-xl">App Ideas</a>
+  </div>
 </header>
 
 <slot />
