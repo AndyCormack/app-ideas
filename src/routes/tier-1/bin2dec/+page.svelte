bb7d843abc637a8da87e474956d6846cea6908e5
Andy Cormack <incurse@gmail.com>
2024-01-27 01:02:27 +0000

    feat: add custom default transition and tidy up error message styles

--- src/routes/tier-1/bin2dec/+page.svelte
+++ src/routes/tier-1/bin2dec/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { flyDefault } from '$/lib/transitions'
   import { bin2Dec } from './bin2dec'
 
   let binary = $state('')
@@ -19,7 +20,7 @@
   <div class="card-body">
     <div class="card-title">Binary to Decimal</div>
 
-    <label class="form-control">
+    <label class="form-control gap-2 w-full max-w-xs">
       <div class="label">
         <span class="label-text">Enter binary</span>
       </div>
@@ -27,14 +28,20 @@
       <input
         type="text"
         placeholder="e.g. 10011101"
-        class="input input-bordered w-full max-w-xs"
+        class="input input-bordered"
         maxlength="32"
         bind:value={binary}
         on:input={onInputChanged}
       />
 
       {#if error}
-        <div>{error}</div>
+        <div
+          in:flyDefault
+          out:flyDefault
+          class="alert p-1 rounded-sm alert-error text-white"
+        >
+          {error}
+        </div>
       {/if}
     </label>
 
