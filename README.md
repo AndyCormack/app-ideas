db37ba4417fb59fc5b46a67edb21b199e8487de8
Andy Cormack <incurse@gmail.com>
2024-01-27 11:48:44 +0000

    feat: add all 8 border radius sliders

--- README.md
+++ README.md
@@ -30,13 +30,13 @@ Working on learning Svelte and SvelteKit by creating the apps listed here: [http
 
   #### User Stories
 
-  - [ ] User can see a box which has a `border-radius` property applied to it
-  - [ ] User can change the 4 `border-radius` values that are applied to the box (top-left, top-right, bottom-left, bottom-right)
+  - [x] User can see a box which has a `border-radius` property applied to it
+  - [x] User can change the 4 `border-radius` values that are applied to the box (top-left, top-right, bottom-left, bottom-right)
   - [ ] User can copy the resulting CSS to the clipboard
 
   #### Bonus features
 
-  - [ ] User can change all 8 possible values of the border-radius in order to create a complex shape
+  - [x] User can change all 8 possible values of the border-radius in order to create a complex shape
 
 ## Developing
 
--- src/routes/tier-1/border-radius-previewer/+page.svelte
+++ src/routes/tier-1/border-radius-previewer/+page.svelte
@@ -1,26 +1,226 @@
 <script lang="ts">
-  let amount = $state(20)
-  let unit = $state('px')
-  let borderRadius = $derived(`${amount}${unit}`)
+  let topLeft = $state({
+    width: 10,
+    height: 20,
+    widthUnit: 'px',
+    heightUnit: 'px',
+  })
+  let topLeftCss = $derived(
+    `${topLeft.width}${topLeft.widthUnit} ${topLeft.height}${topLeft.heightUnit}`
+  )
+
+  let topRight = $state({
+    width: 30,
+    height: 40,
+    widthUnit: 'px',
+    heightUnit: 'px',
+  })
+  let topRightCss = $derived(
+    `${topRight.width}${topRight.widthUnit} ${topRight.height}${topRight.heightUnit}`
+  )
+
+  let bottomLeft = $state({
+    width: 50,
+    height: 60,
+    widthUnit: 'px',
+    heightUnit: 'px',
+  })
+  let bottomLeftCss = $derived(
+    `${bottomLeft.width}${bottomLeft.widthUnit} ${bottomLeft.height}${bottomLeft.heightUnit}`
+  )
+
+  let bottomRight = $state({
+    width: 70,
+    height: 80,
+    widthUnit: 'px',
+    heightUnit: 'px',
+  })
+  let bottomRightCss = $derived(
+    `${bottomRight.width}${bottomRight.widthUnit} ${bottomRight.height}${bottomRight.heightUnit}`
+  )
 </script>
 
 <div class="text-xl">Border-radius Previewer</div>
 
-<div class="card bg-emerald-500 m-8" style:border-radius={borderRadius}>
-  <div class="card-body">
-    <label class="form-control">
-      <div class="label">
-        <span class="label-text">Border Radius</span>
+<div
+  class="card bg-secondary-content m-8"
+  style:border-top-left-radius={topLeftCss}
+  style:border-top-right-radius={topRightCss}
+  style:border-bottom-left-radius={bottomLeftCss}
+  style:border-bottom-right-radius={bottomRightCss}
+>
+  <div class="card-body grid grid-rows-3 grid-cols-2">
+    <!-- Top Left -->
+    <div>
+      <div>
+        <input
+          type="range"
+          bind:value={topLeft.width}
+          min="0"
+          max="100"
+          class="inline-flex w-20 ml-6"
+        />
+
+        <div class="inline-flex flex-row gap-1">
+          <span>{topLeft.width}</span>
+
+          <select class="select select-xs" bind:value={topLeft.widthUnit}>
+            <option value="px">px</option>
+            <option value="%">%</option>
+          </select>
+        </div>
+      </div>
+
+      <div class="inline-flex flex-col items-start gap-2">
+        <input
+          type="range"
+          bind:value={topLeft.height}
+          min="0"
+          max="100"
+          class="vertical w-6 h-20"
+        />
+
+        <div class="flex flex-row gap-1">
+          <span>{topLeft.height}</span>
+
+          <select class="select select-xs" bind:value={topLeft.heightUnit}>
+            <option value="px">px</option>
+            <option value="%">%</option>
+          </select>
+        </div>
+      </div>
+    </div>
+
+    <!-- Top Right -->
+    <div class="flex flex-col items-end">
+      <div class="flex flex-row-reverse">
+        <input
+          type="range"
+          bind:value={topRight.width}
+          min="0"
+          max="100"
+          class="inline-flex w-20 mr-6"
+        />
+
+        <div class="inline-flex flex-row gap-1 items-end">
+          <span>{topRight.width}</span>
+
+          <select class="select select-xs" bind:value={topRight.widthUnit}>
+            <option value="px">px</option>
+            <option value="%">%</option>
+          </select>
+        </div>
+      </div>
+
+      <div class="inline-flex flex-col items-end gap-2">
+        <input
+          type="range"
+          bind:value={topRight.height}
+          min="0"
+          max="100"
+          class="vertical w-6 h-20"
+        />
+
+        <div class="flex flex-row gap-1">
+          <span>{topRight.height}</span>
+
+          <select class="select select-xs" bind:value={topRight.heightUnit}>
+            <option value="px">px</option>
+            <option value="%">%</option>
+          </select>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-span-2 flex items-center justify-center">Border Radius</div>
+
+    <!-- Bottom Left -->
+    <div>
+      <div class="inline-flex flex-col items-start gap-2">
+        <div class="flex flex-row gap-1">
+          <span>{bottomLeft.height}</span>
+
+          <select class="select select-xs" bind:value={bottomLeft.heightUnit}>
+            <option value="px">px</option>
+            <option value="%">%</option>
+          </select>
+        </div>
+
+        <input
+          type="range"
+          bind:value={bottomLeft.height}
+          min="0"
+          max="100"
+          class="vertical w-6 h-20"
+        />
       </div>
 
       <div>
-        <input class="input" type="number" bind:value={amount} />
+        <input
+          type="range"
+          bind:value={bottomLeft.width}
+          min="0"
+          max="100"
+          class="inline-flex w-20 ml-6"
+        />
 
-        <select class="select" bind:value={unit}>
-          <option value="px">px</option>
-          <option value="%">%</option>
-        </select>
+        <div class="inline-flex flex-row gap-1">
+          <span>{bottomLeft.width}</span>
+
+          <select class="select select-xs" bind:value={bottomLeft.widthUnit}>
+            <option value="px">px</option>
+            <option value="%">%</option>
+          </select>
+        </div>
       </div>
-    </label>
+    </div>
+
+    <!-- Bottom Right -->
+    <div class="flex flex-col items-end">
+      <div class="inline-flex flex-col items-end gap-2">
+        <div class="flex flex-row gap-1">
+          <span>{bottomRight.height}</span>
+
+          <select class="select select-xs" bind:value={bottomRight.heightUnit}>
+            <option value="px">px</option>
+            <option value="%">%</option>
+          </select>
+        </div>
+
+        <input
+          type="range"
+          bind:value={bottomRight.height}
+          min="0"
+          max="100"
+          class="vertical w-6 h-20"
+        />
+      </div>
+
+      <div class="flex flex-row-reverse">
+        <input
+          type="range"
+          bind:value={bottomRight.width}
+          min="0"
+          max="100"
+          class="inline-flex w-20 mr-6"
+        />
+
+        <div class="inline-flex flex-row gap-1 items-end">
+          <span>{bottomRight.width}</span>
+
+          <select class="select select-xs" bind:value={bottomRight.widthUnit}>
+            <option value="px">px</option>
+            <option value="%">%</option>
+          </select>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
+
+<style>
+  .vertical {
+    appearance: slider-vertical;
+    orient: vertical;
+  }
+</style>
